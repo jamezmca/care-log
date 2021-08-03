d03dc9de0ca99b094feb57c7d939e96ec0edf161
James <jamesmca@live.com>
2021-08-03 14:03:10 +0100

    trying to make the thing turn into a pointer cusor

--- src/components/dashboardcomponents/dayview/Card.js
+++ src/components/dashboardcomponents/dayview/Card.js
@@ -2,16 +2,21 @@ import React from 'react'
 import * as dayview from './dayview.module.css'
 
 
-export default function Card({ cardType, event }) {
+export default function Card({ cardType, event, editDelete }) {
     function backgroundColorSelector(cardType) {
         if (cardType === 'reminder') return "radial-gradient(#1fe4f5, #3fbafe)"
         if (cardType === 'note') return "radial-gradient(#76b2fe, #b69efe)"
         if (cardType === 'goal') return "radial-gradient(#60efbc, #58d5c9)"
     }
 
+    let cursorPointer = editDelete ? 'pointer' : 'auto'
+
     //for note content search for new line and add only one new line
     const noteContent = (
-        <div className={dayview.cardContainer} style={{ background: backgroundColorSelector(cardType) }}>
+        <div className={dayview.cardContainer} style={{ background: backgroundColorSelector(cardType), cursor: cursorPointer }}>
+            {editDelete && <div style={{ display: 'flex', backgroundColor: 'transparent', justifyContent: 'space-between' }}>
+                <div style={{ backgroundColor: 'transparent' }}><p >x</p></div>
+            </div>}
             <div className={dayview.noteContainer}>
                 {event.title.split("\n").length > 1 ?
                     event.title.split('\n').map((note, index) => {
@@ -26,6 +31,7 @@ export default function Card({ cardType, event }) {
     const reminderContent = (
         <div className={dayview.cardContainer} style={{ background: backgroundColorSelector(cardType) }}>
             <div className={dayview.reminderContainer}>
+                { }
                 <div style={{ fontSize: '15px', fontWeight: '500', backgroundColor: 'transparent', display: 'flex', justifyContent: 'space-between', marginBottom: '4px', borderBottom: '1px solid white' }}>
                     <p >{event.title.toUpperCase()}</p> <p>{event.time}</p>
                 </div>
--- src/components/dashboardcomponents/dayview/DayView.js
+++ src/components/dashboardcomponents/dayview/DayView.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 import * as dayview from './dayview.module.css'
 
 
 export default function DayView({ clicked, nav, days, eventsForClickedDay, hourSelected, setHourSelected }) {
+    const [editDelete, setEditDelete] = useState(false)
     // default colors palegreen, paleturquoise, thistle
     console.log(eventsForClickedDay)
-    let redOrderedEventsForDay = [eventsForClickedDay.filter(event => event.type === "reminder"), eventsForClickedDay.filter(event => event.type === "goal"), eventsForClickedDay.filter(event => event.type === "note"),]
+    let redOrderedEventsForDay = [...eventsForClickedDay.filter(event => event.type === "reminder"), ...eventsForClickedDay.filter(event => event.type === "goal"), ...eventsForClickedDay.filter(event => event.type === "note")]
     //filter events and then add them back together in a new filtered array - reminders first goals second notes third
-
+    console.log(redOrderedEventsForDay)
     return (
         <div className={dayview.container}>
-            {eventsForClickedDay.map((event) => {
-                return <Card key={event.id} event={event} cardType={event.type}/>
+            <div onClick={() => setEditDelete(!editDelete)} className={dayview.editButton}>
+                <p>EDIT / DELETE</p>
+            </div>
+            {redOrderedEventsForDay.map((event) => {
+                return <Card key={event.id} event={event} cardType={event.type}  editDelete={editDelete}/>
             })}
+            {eventsForClickedDay.length === 0 && <p style={{ alignSelf: 'center', marginTop: '20px'}}>No plans for today Ü</p>}
         </div>
     )
 }
